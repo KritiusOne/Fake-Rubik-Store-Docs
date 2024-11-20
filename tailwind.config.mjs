c8e21d6ac399d6179f91b45c34b93a578356e7ee
Julio Rodríguez <julio10489@gmail.com>
2024-11-20 15:09:38 -0500

    Added:  config to theme colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"black": "#080705",
+				"charcoal": "#40434E",
+				"cordovan": "#912F40",
+				"redWood": "#B63E52",
+				"babyPowder": "#FFFFFA"
+			}
+		},
 	},
 	plugins: [],
 }
