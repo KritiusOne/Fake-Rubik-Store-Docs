7c8c91f1b113efad1981d1d1387ee2d0c68e434b
Julio Rodríguez <julio10489@gmail.com>
2024-11-20 23:12:59 -0500

    Added: info home page example

---
+++ src/lib/productResponseExample.ts
@@ -0,0 +1,34 @@
+export const getAllProducts = `{
+  "metaData": {
+    "totalCount": 31,
+    "pageSize": 3,
+    "currentPage": 1,
+    "totalPage": 2,
+    "hasNextPage": true,
+    "hasPreviousPage": false,
+    "nextPageURL": "/api/Product?MinPrice=&MaxPrice=&PageSize=20&PageNumber=2",
+    "previousPageURL": "/api/Product?MinPrice=&MaxPrice=&PageSize=20&PageNumber=false"
+  },
+  "statusCode": 200,
+  "msg": "This is all products",
+  "response": [
+    {
+    "id": 2,
+    "name": "GAN Mirror M (Magnético) UV 3×3 Morado",
+    "price": 120000,
+    "thumbnail": "39e50337-fe40-4683-88fb-b440d1bdb9a7"
+    },
+    {
+    "id": 1002,
+    "name": "QiYi Warrior 3x3 S",
+    "price": 12900,
+    "thumbnail": "922de203-de10-404d-b8bc-5c503ca24a61"
+    },
+    {
+    "id": 1003,
+    "name": "MoYu Super RS3 M 2022",
+    "price": 66900,
+    "thumbnail": "79f4fa59-c04c-481b-98b3-86bad3f5af6b"
+    }
+  ]
+}`
