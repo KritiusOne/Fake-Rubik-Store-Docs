76872714ddb86d02ef4613f5d344fe273511c784
Julio David Rodríguez Ureche <julio10490@gmail.com>
2024-12-03 23:42:13 -0500

    Added: Auth page examples code

---
+++ src/lib/AuthExamples.ts
@@ -0,0 +1,19 @@
+export const createUserCSharp = `
+    public class CreateUserDTO
+    {
+        public string? Name { get; set; }
+        public string? SecondName { get; set; }
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+`
+export const createUserTs = `
+export interface CreateUser {
+  Name?: string
+  SecondName?: string
+  Email: string
+  Phone: string
+  Password: string
+}
+`
