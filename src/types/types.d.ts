223a34940fdbb430a68d9cc8805aa1887721f701
Julio David Rodríguez Ureche <julio10490@gmail.com>
2024-12-11 11:59:49 -0500

    Added: Types entity

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,1 +1,32 @@
 export type HTTPUsedVerbs = 'GET' | 'POST' | 'PUT' | 'DELETE'
+
+export interface entity {
+  EntityName: string
+  description: string
+  propierties: propierty[]
+  relationships: relationships[]
+}
+export interface propierty {
+  propiertyName: string
+  dataType: string
+  description: string
+  isFK: boolean
+  isPK?: boolean
+}
+export interface relationships {
+  typeRelation: TypeRelation
+  key: string
+  tableRelation: Entities
+}
+export type TypeRelation = "Muchos a muchos" | "Uno a muchos" | "Uno a uno"
+export type Entities = 
+  | "Categories"
+  | "Address"
+  | "Deliveries"
+  | "Orders"
+  | "Products"
+  | "Reviews"
+  | "Role"
+  | "States"
+  | "User"
+  | "ProductCategory";
