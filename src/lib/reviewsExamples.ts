60328d82d6b3cdbaee7150b917214101c09a556c
Julio David Rodríguez Ureche <julio10490@gmail.com>
2024-12-16 22:49:01 -0500

    added: add content about reviews endpoint and examples codes about reviews

---
+++ src/lib/reviewsExamples.ts
@@ -0,0 +1,21 @@
+export const createReview = `
+{
+  "productId": 0,
+  "userId": 0,
+  "description": "string",
+  "rate": 0
+}
+`
+export const getAllReviews = `
+{
+  "msg": "This is the all reviews",
+  "response": [
+    {
+      "productId": 2,
+      "userId": 1004,
+      "description": "Es un gran cubo, muy buen material, muy lindo y tiene gran giro",
+      "rate": 5
+    }
+  ]
+}
+`
