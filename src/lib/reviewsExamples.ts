8fb78e593f41d47cfea420f903fb56b04bf0b4d9
Julio David Rodríguez Ureche <julio10490@gmail.com>
2024-12-21 16:40:31 -0500

    Added: add new content about endpoints from review entity

--- src/lib/reviewsExamples.ts
+++ src/lib/reviewsExamples.ts
@@ -19,3 +19,23 @@ export const getAllReviews = `
   ]
 }
 `
+
+export const getReviewById = `
+{
+  "msg": "This is the reviews from the this ID",
+  "response": {
+    "productId": 2,
+    "userId": 1004,
+    "description": "Es un gran cubo, muy buen material, muy lindo y tiene gran giro",
+    "rate": 5
+  }
+}
+`
+export const postReviwBody = `
+export interface CreateReview {
+  productId: number,
+  userId: number,
+  description: string,
+  rate: number
+}
+`
