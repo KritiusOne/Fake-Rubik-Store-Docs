66ff5bda403ca0a8c6a3108cd985d0f6a351ab57
Julio David Rodríguez Ureche <julio10490@gmail.com>
2024-12-29 16:48:47 -0500

    Added: add info about orders, query filters

--- src/lib/ordersExamples.ts
+++ src/lib/ordersExamples.ts
@@ -55,3 +55,17 @@ export const getAll = `
    ]
 }
 `;
+
+export const queryFilters = `
+    public class OrderQueryFilters
+    {
+        public int PageSize { get; set; } = 20;
+        public int PageNumber { get; set; } = 1;
+        public DateTime? Date { get; set; }
+        public DateTime? MinDate { get; set; }
+        public DateTime? MaxDate { get; set; }
+        public double? MinPrice { get; set; }
+        public double? MaxPrice { get; set; }
+        public int? IdUser { get; set; }
+    }
+`;
