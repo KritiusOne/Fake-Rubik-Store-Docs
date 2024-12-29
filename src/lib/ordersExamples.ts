5ea507958553a86a66c4165689d4c2c946d038d1
Julio David Rodríguez Ureche <julio10490@gmail.com>
2024-12-29 17:33:16 -0500

    added: add info about body from create order endpoint

--- src/lib/ordersExamples.ts
+++ src/lib/ordersExamples.ts
@@ -69,3 +69,61 @@ export const queryFilters = `
         public int? IdUser { get; set; }
     }
 `;
+
+export const getById = `
+{
+	"deliveryInfo": {
+		"idState": 3,
+		"idUser": 2012,
+		"code": "18fa3853-1af9-4a12-a183-6cadcfc4d5c3"
+	},
+	"userInfo": {
+		"id": 2012,
+		"idRole": 2,
+		"idAddress": 2011,
+		"name": "Thor",
+		"secondName": "Odinson",
+		"email": "principe@asgard.com"
+	},
+	"orderProducts": [
+		{
+			"idProduct": 2,
+			"idOrder": 1,
+			"productsNumber": 1,
+			"price": 120000,
+			"numberCard": null,
+			"productInfo": {
+				"id": 2,
+				"name": "GAN Mirror M (Magnético) UV 3×3 Morado",
+				"price": 120000,
+				"stock": 10,
+				"image": "eadc4650-7dab-4f70-9657-2354615566ed",
+				"description": "Nuevo desafío: GAN MirrorM es un nuevo diseño visual con un cubo de velocidad de 3 x 3, que no se resuelve con colores sino por formas. El cubo de espejo GAN te traerá más diversión",
+				"thumbnail": "39e50337-fe40-4683-88fb-b440d1bdb9a7"
+			}
+		}
+	],
+	"id": 1,
+	"idUser": 2012,
+	"idDelivery": 2,
+	"date": "2024-07-22T17:50:04.367",
+	"finalPrice": 120000,
+	"numberCard": "1234567890"
+}
+`;
+
+export const createOrder = `
+{
+  "idUser": 0,
+  "date": "2024-12-29T22:27:48.194Z",
+  "finalPrice": 0,
+  "numberCard": "string",
+  "orderProducts": [
+    {
+      "idProduct": 0,
+      "productsNumber": 0,
+      "price": 0
+    }
+  ]
+}
+`;
