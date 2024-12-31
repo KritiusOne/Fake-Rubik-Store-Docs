dfc495d3dba589503f1055de22d55dfce9c36220
Julio David Rodríguez Ureche <julio10490@gmail.com>
2024-12-30 22:32:16 -0500

    added: add examples about state entity

---
+++ src/lib/statesExamples.ts
@@ -0,0 +1,31 @@
+export const getAll = `{
+  "msg": "This is the all states of deliveries",
+  "response": [
+    {
+      "id": 1,
+      "name": "NO ATENDIDO"
+    },
+    {
+      "id": 2,
+      "name": "EN PREPARACIÓN"
+    },
+    {
+      "id": 3,
+      "name": "ENVIADO"
+    },
+    {
+      "id": 4,
+      "name": "RECIBIDO"
+    },
+    {
+      "id": 5,
+      "name": "CANCELADO"
+    }
+  ]
+}`;
+
+export const createState = `
+{
+  "name": "string"
+}
+`;
